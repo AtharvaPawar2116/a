dee943aa67577bf49cc1024644f581ae0acb3396
AtharvaPawar2116 <132532065+AtharvaPawar2116@users.noreply.github.com>
2025-11-05 17:18:01 +0530

    Update 3_Bank_account.sol

--- 3_Bank_account.sol
+++ 3_Bank_account.sol
@@ -1,38 +1,32 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract BankAccount {
-    mapping(address => uint256) private balances;
-    mapping(address => bool) private isUser;
+contract Bank {
+    uint256 balance = 0;
+    address public accOwner;
 
-    event AccountCreated(address user, uint256 amount);
-    event Deposit(address user, uint256 amount);
-    event Withdraw(address user, uint256 amount);
-
-    function createAccount() public payable {
-        require(!isUser[msg.sender], "Account exists");
-        isUser[msg.sender] = true;
-        balances[msg.sender] = msg.value;
-        emit AccountCreated(msg.sender, msg.value);
+    // Constructor - runs only once when contract is deployed
+    constructor() {
+        accOwner = msg.sender;
     }
 
-    function deposit() public payable {
-        require(isUser[msg.sender], "No account");
-        require(msg.value > 0, "Amount > 0");
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
+    // Deposit Function
+    function Deposit() public payable {
+        require(accOwner == msg.sender, "You are not an account owner");
+        require(msg.value > 0, "Amount should be greater than 0!");
+        balance += msg.value;
     }
 
-    function withdraw(uint256 amount) public {
-        require(isUser[msg.sender], "No account");
-        require(balances[msg.sender] >= amount, "Low balance");
-        balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
-        emit Withdraw(msg.sender, amount);
+    // Withdraw Function
+    function Withdraw() public payable {
+        require(accOwner == msg.sender, "You are not an account owner");
+        require(msg.value > 0, "Withdrawal money should be greater than 0.");
+        balance -= msg.value;
     }
 
+    // Show Balance Function
     function showBalance() public view returns (uint256) {
-        require(isUser[msg.sender], "No account");
-        return balances[msg.sender];
+        require(accOwner == msg.sender, "You are not an account owner");
+        return balance;
     }
 }
